0484b7044d9575f9defe511d58aa68f9374c81d4
Top <weerapat.thawatchoketawee@gmail.com>
2018-03-30 21:56:18 +0700

    Deploy gh-pages

---
+++ out/_next/d3be1698-2cc2-4d7e-b0df-85ba20b34081/page/posts.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/posts', function() {
+            var comp = module.exports=webpackJsonp([4],{283:function(e,t,r){e.exports=r(284)},284:function(e,t,r){"use strict";function n(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var u=r(18),a=n(u),l=r(22),f=n(l),o=r(118),c=n(o),d=r(0),p=n(d),i=function(e){var t=e.pathname,r=e.query;return p.default.createElement("div",null,p.default.createElement("pre",null,(0,c.default)(t)),p.default.createElement("pre",null,(0,c.default)(r)))};i.getInitialProps=function(){var e=(0,f.default)(a.default.mark(function e(t){var r=t.pathname,n=t.query;return a.default.wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.abrupt("return",{pathname:r,query:n});case 1:case"end":return e.stop()}},e,void 0)}));return function(t){return e.apply(this,arguments)}}(),t.default=i}},[283]);
+            return { page: comp.default }
+          })
+        
